5631546b735733913d0e5df0fc3567cc27a324d6
Matt Esch <mattesch@uber.com>
2015-07-12 23:59:48 -0700

    add full call req example

--- index.js
+++ index.js
@@ -20,24 +20,137 @@ function parseFrame(buffer) {
 }
 
 /*
-----------------------------------------------
-| 0-7  | size:2 | type:1 | reserved:1 | id:4 |
-|------|-------------------------------------|
-| 8-15 | reserved:8                          |
-|------|-------------------------------------|
-| 16+  | payload - based on type             |
-----------------------------------------------
+flags:1 ttl:4 tracing:25
+service~1 nh:1 (hk~1 hv~1){nh}
+csumtype:1 (csum:4){0,1} arg1~2 arg2~2 arg3~2
+*/
+
+/*
+-----------------------------------------------
+|  0-7  | size:2 | type:1 | reserved:1 | id:4 |
+|-------|-------------------------------------|
+|  8-15 | reserved:8                          |
+|-------|-------------------------------------|
+| 16-23 | flags:1 | ttl:4 | tracing:3         |
+|-------|-------------------------------------|
+| 24-31 | tracing:8                           |
+|-------|-------------------------------------|
+| 32-39 | tracing:8                           |
+|-------|-------------------------------------|
+| 40-47 | tracing:6 | serviceLen:1 | dynamic..|
+-----------------------------------------------
+
 */
 
 var b = new Buffer([
-    0x10, 0x00,             // size 16 - Big Endian
+    0x00, 0x34,             // size 52 - Big Endian
     0x03,                   // type 3 (call request)
     0x00,                   // reserved
     0xFF, 0xFF, 0xFF, 0xFE, // id (max non-reserved value)
     0x00, 0x00, 0x00, 0x00, // reserved
-    0x00, 0x00, 0x00, 0x00  // reserved
+    0x00, 0x00, 0x00, 0x00, // reserved
+    0x08,                   // flags
+    0x01, 0x02, 0x03, 0x04,  // TTL Big Endian
+
+    // Tracing 25 bytes
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00,
+
+    // Service name: 5 bytes - moose
+    0x05, 0x6D, 0x6F, 0x6F, 0x73, 0x65,
+
+    // Headers: 3
+    0x03,
+
+    // header1: "header1string"
+    0x07, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x31,
+    0x0D, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x31,
+    0x73, 0x74, 0x72, 0x69, 0x6E, 0x67,
+
+    // header2: "header2string"
+    0x07, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x32,
+    0x0D, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x32,
+    0x73, 0x74, 0x72, 0x69, 0x6E, 0x67,
+
+    // auth: "moose"
+    0x04, 0x61, 0x75, 0x74, 0x68,
+    0x05, 0x6D, 0x6F, 0x6F, 0x73, 0x65,
+
+    // Checksum (type 0 no checksum)
+    0x00,
+
+    // arg1: length 1
+    0x00, 0x01, 0xFF,
+
+    // arg2: length 2
+    0x00, 0x02, 0xFF, 0xFE,
+
+    // arg3: length 3
+    0x00, 0x03, 0xFF, 0xFE, 0xFD
 ]);
 
-var frame = parseFrame(b);
+var frame = fillBuffers(parseFrame(b), b);
 
 console.log(frame);
+
+function fillBuffers(rawFrame, buffer) {
+    if (!rawFrame ||
+        rawFrame.type !== 3 ||
+        !rawFrame.body ||
+        buffer.length < 46
+    ) {
+        return rawFrame;
+    }
+
+    var body = rawFrame.body;
+    var tracing = rawFrame.body.tracing;
+
+    if (tracing) {
+        tracing.spanid = buffer.slice(21, 29);
+        tracing.parentid = buffer.slice(29, 37);
+        tracing.traceid = buffer.slice(37, 45);
+    }
+
+    var argpos = body.argstart;
+
+    if (!argpos) {
+        return rawFrame;
+    }
+
+    var args = body.ags = [];
+
+    var arg1len = body.arg1len;
+    var arg2len = body.arg2len;
+    var arg3len = body.arg3len;
+
+    argpos += 2;
+
+    if (!arg1len) {
+        return rawFrame;
+    }
+
+    args.push(buffer.slice(argpos, argpos + arg1len));
+    argpos += arg1len + 2;
+
+    if (!arg2len) {
+        return rawFrame;
+    }
+
+    args.push(buffer.slice(argpos, argpos + arg2len));
+    argpos += arg2len + 2;
+
+    if (!arg3len) {
+        return rawFrame;
+    }
+
+    args.push(buffer.slice(argpos, argpos + arg3len));
+
+    delete body.argstart;
+    delete body.arg1len;
+    delete body.arg2len;
+    delete body.arg3len;
+
+    return rawFrame;
+}
--- tchannel_parser.cc
+++ tchannel_parser.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <nan.h>
 
 using namespace v8;
@@ -39,38 +38,8 @@ namespace TChannel {
         TC_ERROR_TYPE=0xff
     };
 
-    /* TODO: maybe write a proper C(++) parser
-    struct frame {
-        uint16_t size;
-        FrameType type;
-        uint32_t id;
-    };
-
-    struct call_req_frame {
-        uint16_t size;
-        FrameType type;
-        uint32_t id;
-        uint8_t flags;
-        uint32_t ttl;
-        // etc..
-    }
-
-
-    void readCallReqFrame(
-        struct call_req_frame* frame,
-        char* bufferData,
-        size_t length
-    ) {
-        frame->frameType = FrameType.CALL_REQ;
-        memcpy(&frame->size, bufferData, 2);
-
-        // etc ..
-    }
-    */
-
     inline uint16_t readFrameSize(char* buffer) {
-        // Note: Assuming architecture is little endian
-        return ((unsigned char)buffer[1] << 8) | ((unsigned char)buffer[0]);
+        return ((unsigned char)buffer[0] << 8) | ((unsigned char)buffer[1]);
     }
 
     inline FrameType readFrameType(char* buffer) {
@@ -79,32 +48,406 @@ namespace TChannel {
 
     inline uint32_t readFrameId(char* buffer) {
         // Note: Assuming architecture is little endian
-        return ((unsigned char)buffer[4] << 24) |
+        return (uint32_t)(
+            ((unsigned char)buffer[4] << 24) |
             ((unsigned char)buffer[5] << 16) |
             ((unsigned char)buffer[6] << 8) |
-            ((unsigned char)buffer[7]);
+            ((unsigned char)buffer[7])
+        );
     }
 }
 
+static Persistent<String> args_sym;
+static Persistent<String> body_sym;
+static Persistent<String> csum_sym;
+static Persistent<String> flags_sym;
+static Persistent<String> headers_sym;
+static Persistent<String> id_sym;
+static Persistent<String> service_sym;
+static Persistent<String> size_sym;
+static Persistent<String> tracing_sym;
+static Persistent<String> ttl_sym;
+static Persistent<String> type_sym;
+static Persistent<String> val_sym;
+static Persistent<String> argstart_sym;
+static Persistent<String> arg1len_sym;
+static Persistent<String> arg2len_sym;
+static Persistent<String> arg3len_sym;
+
+
+
+inline uint8_t readUint8(char* buffer, size_t offset) {
+    return (uint8_t)buffer[offset];
+}
+
+inline uint16_t readUint16BE(char* buffer, size_t offset) {
+    return (uint16_t)(
+        ((unsigned char)buffer[offset] << 8) |
+        ((unsigned char)buffer[offset + 1])
+    );
+}
+
+inline uint32_t readUint32BE(char* buffer, size_t offset) {
+    return (uint32_t)(
+        ((unsigned char)buffer[offset] << 24) |
+        ((unsigned char)buffer[offset + 1] << 16) |
+        ((unsigned char)buffer[offset + 2] << 8) |
+        ((unsigned char)buffer[offset + 3])
+    );
+}
+
+inline uint32_t readUint8StringToObject(
+    Local<Object> obj,
+    Persistent<String> key,
+    char* buffer,
+    size_t length,
+    size_t offset
+) {
+    uint8_t stringLength = readUint8(buffer, offset);
+    size_t newOffset = offset + 1 + stringLength;
+
+    if (newOffset <= length) {
+        obj->Set(
+            key,
+            NanNew<String>(&buffer[offset + 1], stringLength)
+        );
+    }
+
+    return newOffset;
+}
+
+inline uint32_t readUint8StringMapToObject(
+    Local<Object> obj,
+    char* buffer,
+    size_t length,
+    size_t offset
+) {
+    uint8_t keyLength = readUint8(buffer, offset);
+    size_t valueOffset = offset + 1 + keyLength;
+
+    if (valueOffset >= length) {
+        return valueOffset;
+    }
+
+    size_t valueLength = readUint8 (buffer, valueOffset);
+    size_t newOffset = valueOffset + 1 + valueLength;
+
+    if (newOffset >= length) {
+        return newOffset;
+    }
+
+    Local<String> key = NanNew<String>(&buffer[offset + 1], keyLength);
+    Local<String> value = NanNew<String>(&buffer[valueOffset + 1], valueLength);
+
+    obj->Set(key, value);
+
+    return newOffset;
+}
+
+inline uint32_t readUint16BufferLengthToObject(
+    Local<Object> obj,
+    Persistent<String> key,
+    char* buffer,
+    size_t length,
+    size_t offset
+) {
+    uint16_t bufferLength = readUint16BE(buffer, offset);
+    size_t newOffset = offset + 2 + bufferLength;
+
+    if (newOffset <= length) {
+        obj->Set(
+            key,
+            NanNew<Uint32>((uint32_t)bufferLength)
+        );
+    }
+
+    return newOffset;
+}
+
 // No intermediate struct parsing, direct js object creation
-inline void readTChannelReqFrame(
+inline void readTChannelHeader(
     Local<Object> frame,
+    TChannel::FrameType frameType,
     char* buffer,
     size_t length
 ) {
     // size:2
     uint32_t frameSize = TChannel::readFrameSize(buffer);
-    frame->Set(NanNew<String>("size"), NanNew<Uint32>(frameSize));
+    frame->Set(size_sym, NanNew<Uint32>(frameSize));
 
     // type:1 (we know it's a call request in this case)
+    frameType = TChannel::readFrameType(buffer);
     frame->Set(
-        NanNew<String>("type"),
+        type_sym,
         NanNew<Uint32>((uint32_t)TChannel::TC_CALL_REQ_TYPE)
     );
 
     // id:4
     uint32_t frameId = TChannel::readFrameId(buffer);
-    frame->Set(NanNew<String>("id"), NanNew<Uint32>(frameId));
+    frame->Set(id_sym, NanNew<Uint32>(frameId));
+}
+
+/*
+{
+    size: Number,
+    type: Number,
+    id: Number
+}
+
+function readCallReqFrom(buffer, offset) {
+    var res;
+    var body = new CallRequest();
+
+    // flags:1 - Number
+    res = bufrw.UInt8.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    body.flags = res.value;
+
+    // ttl:4 - Number
+    res = bufrw.UInt32BE.readFrom(buffer, offset);
+    if (res.err) return res;
+
+    if (res.value <= 0) {
+        return bufrw.ReadResult.error(errors.InvalidTTL({
+            ttl: res.value
+        }), offset, body);
+    }
+
+    offset = res.offset;
+    body.ttl = res.value;
+
+    // tracing:24 traceflags:1
+    res = Tracing.RW.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    body.tracing = res.value;
+
+    // service~1 String (first byte string length)
+    res = bufrw.str1.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    body.service = res.value;
+
+    
+    // nh:1 (hk~1 hv~1){nh} Dictionary key-value string-string
+    res = header.header1.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    body.headers = res.value;
+
+
+    // Object: { type: Number, val: Number | 0 }
+    // csumtype:1 (csum:4){0,1} (arg~2)*
+    res = argsrw.readFrom(body, buffer, offset);
+    if (!res.err) res.value = body;
+
+    return res;
+}
+*/
+
+/*
+function readTracingFrom(buffer, offset) {
+    var tracing = new Tracing();
+    var res;
+
+    // Buffer (64bit number)
+    res = fix8.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    tracing.spanid = res.value;
+
+    // ditto
+    res = fix8.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    tracing.parentid = res.value;
+
+    // ditto
+    res = fix8.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    tracing.traceid = res.value;
+
+    // Number
+    res = bufrw.UInt8.readFrom(buffer, offset);
+    if (res.err) return res;
+    offset = res.offset;
+    tracing.flags = res.value;
+
+    return bufrw.ReadResult.just(offset, tracing);
+}
+*/
+
+/*
+ body: {
+    type: Number,
+    flags: Number,
+    ttl: Number,
+    tracing: {
+        spanid: Buffer,
+        parentid: Buffer,
+        traceid: Buffer,
+        flags: Number,
+    },
+    service: String,
+    headers: Object<String, String>,
+    csum: {
+        type: Number,
+        val: Number | 0
+    },
+    args: Array<Buffer>
+}
+*/
+
+/*
+flags:1 ttl:4 tracing:25
+service~1 nh:1 (hk~1 hv~1){nh}
+csumtype:1 (csum:4){0,1} arg1~2 arg2~2 arg3~2
+*/
+inline void readTChannelCallReq(
+    Local<Object> frame,
+    char* buffer,
+    size_t length
+) {
+    // Minimum call req size is 55 bytes
+    if (length < 55) {
+        // TODO: return error
+        return;
+    }
+
+    Local<Object> body = NanNew<Object>();
+    frame->Set(body_sym, body); 
+
+    // Apparently we want this repeated for convenience
+    // type:1 (we know it's a call request in this case)
+    body->Set(
+        type_sym,
+        NanNew<Uint32>((uint32_t)TChannel::TC_CALL_REQ_TYPE)
+    );
+
+    // flags:1
+    uint8_t flags = readUint8(buffer, 16);
+    body->Set(flags_sym, NanNew<Uint32>((uint32_t)flags));
+
+    // ttl:4
+    uint32_t ttl = readUint32BE(buffer, 17);
+    body->Set(ttl_sym, NanNew<Uint32>(ttl));
+
+    // Read tracing info bytes 21 to 45
+    Local<Object> tracing = NanNew<Object>();
+    body->Set(tracing_sym, tracing);
+    uint8_t traceFlags = readUint8(buffer, 45);
+    tracing->Set(flags_sym, NanNew<Uint32>((uint32_t)traceFlags));
+
+    size_t offset = 46; // Dynamic positions begin
+
+    // service~1
+    offset = readUint8StringToObject(
+        body,
+        service_sym,
+        buffer,
+        length,
+        offset
+    );
+
+    if (offset >= length) {
+        // TODO: Error
+        return;
+    }
+
+    // nh:1 (hk~1 hv~1){nh}
+    Local<Object> headers = NanNew<Object>();
+    body->Set(headers_sym, headers);
+
+    uint8_t headerCount = readUint8(buffer, offset);
+    offset += 1;
+
+    if (offset >= length) {
+        // TODO: Error
+        return;
+    }
+
+    for (uint8_t i = 0; i < headerCount; i++) {
+        offset = readUint8StringMapToObject(
+            headers,
+            buffer,
+            length,
+            offset
+        );
+
+        if (offset >= length) {
+            // TODO: Error
+            return;
+        }
+    }
+
+    // csumtype:1 (csum:4){0,1}
+    uint8_t csumtype = readUint8(buffer, offset);
+    offset += 1;
+
+    uint32_t csumval = 0;
+
+    if (csumtype != 0x00) {
+        // check we have 4 bytes of headroom
+        if (offset + 4 >= length) {
+            // TODO: Error
+            return;
+        }
+
+        csumval = readUint32BE(buffer, offset);
+        offset += 4;
+    }
+
+    Local<Object> csum = NanNew<Object>();
+    body->Set(csum_sym, csum);
+    csum->Set(type_sym, NanNew<Uint32>(((uint32_t)csumtype)));
+    csum->Set(val_sym, NanNew<Uint32>(csumval));
+
+    // Check 2 byte overhead for arg1 length
+    if (offset + 1 >= length) {
+        // TODO: Error
+        return;
+    }
+
+    body->Set(argstart_sym, NanNew<Uint32>((uint32_t)offset));
+
+    // arg1
+    offset = readUint16BufferLengthToObject(
+        body,
+        arg1len_sym,
+        buffer,
+        length,
+        offset
+    );
+
+    if (offset + 1 >= length) {
+        // TODO: Error
+        return;
+    }
+
+    // arg2
+    offset = readUint16BufferLengthToObject(
+        body,
+        arg2len_sym,
+        buffer,
+        length,
+        offset
+    );
+
+    if (offset + 1 >= length) {
+        // TODO: Error
+        return;
+    }
+
+    // arg3
+    offset = readUint16BufferLengthToObject(
+        body,
+        arg3len_sym,
+        buffer,
+        length,
+        offset
+    );
 }
 
 NAN_METHOD(Parse) {
@@ -127,7 +470,8 @@ NAN_METHOD(Parse) {
     switch (frameType) {
         case TChannel::TC_CALL_REQ_TYPE:
             frame = NanNew<Object>();
-            readTChannelReqFrame(frame, bufferData, bufferLength);
+            readTChannelHeader(frame, frameType, bufferData, bufferLength);
+            readTChannelCallReq(frame, bufferData, bufferLength);
             NanReturnValue(frame);
         default:
             // TODO: Implement the rest of the frames
@@ -140,6 +484,23 @@ void Init(Handle<Object> exports) {
         NanNew("parse"),
         NanNew<FunctionTemplate>(Parse)->GetFunction()
     );
+
+    NanAssignPersistent(args_sym, NanNew<String>("args"));
+    NanAssignPersistent(body_sym, NanNew<String>("body"));
+    NanAssignPersistent(csum_sym, NanNew<String>("csum"));
+    NanAssignPersistent(flags_sym, NanNew<String>("flags"));
+    NanAssignPersistent(headers_sym, NanNew<String>("headers"));
+    NanAssignPersistent(id_sym, NanNew<String>("id"));
+    NanAssignPersistent(service_sym, NanNew<String>("service"));
+    NanAssignPersistent(size_sym, NanNew<String>("size"));
+    NanAssignPersistent(tracing_sym, NanNew<String>("tracing"));
+    NanAssignPersistent(ttl_sym, NanNew<String>("ttl"));
+    NanAssignPersistent(type_sym, NanNew<String>("type"));
+    NanAssignPersistent(val_sym, NanNew<String>("val"));
+    NanAssignPersistent(argstart_sym, NanNew<String>("argstart"));
+    NanAssignPersistent(arg1len_sym, NanNew<String>("arg1len"));
+    NanAssignPersistent(arg2len_sym, NanNew<String>("arg2len"));
+    NanAssignPersistent(arg3len_sym, NanNew<String>("arg3len"));
 }
 
 NODE_MODULE(tchannel_parser, Init);
